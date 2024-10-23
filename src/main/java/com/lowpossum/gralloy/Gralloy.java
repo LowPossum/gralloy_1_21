43e6e53f440a7e5e8c5a91a4d76bc5b68dafd81d
Low Possum <low1possum@gmail.com>
2024-10-23 19:45:38 +0200

    First items added

--- src/main/java/com/lowpossum/gralloy/Gralloy.java
+++ src/main/java/com/lowpossum/gralloy/Gralloy.java
@@ -1,5 +1,6 @@
 package com.lowpossum.gralloy;
 
+import com.lowpossum.gralloy.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class Gralloy implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/com/lowpossum/gralloy/item/ModItems.java
@@ -0,0 +1,35 @@
+package com.lowpossum.gralloy.item;
+
+import com.lowpossum.gralloy.Gralloy;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    //Create the new ingot
+    public static final Item GRALLOY_INGOT = registerItem("gralloy_ingot", new Item(new Item.Settings()));
+    //Create the new sword
+    public static final Item GRALLOY_SWORD = registerItem("gralloy_sword", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Gralloy.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+
+        Gralloy.LOGGER.info("Registering Mod Items for " + Gralloy.MOD_ID);
+
+        //Add item to the INGREDIENT group
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(GRALLOY_INGOT);});
+
+        //Add item to the COMBAT group
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(GRALLOY_SWORD);});
+    }
+
+}
