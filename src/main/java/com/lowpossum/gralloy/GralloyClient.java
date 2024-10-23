1db6e58c79e5b7ac45c8ce076526616eed54505d
Low Possum <low1possum@gmail.com>
2024-10-23 18:17:22 +0200

    yes

---
+++ src/main/java/com/lowpossum/gralloy/GralloyClient.java
@@ -0,0 +1,10 @@
+package com.lowpossum.gralloy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GralloyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
